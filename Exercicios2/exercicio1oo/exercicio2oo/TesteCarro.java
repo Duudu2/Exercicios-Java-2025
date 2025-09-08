3f743fb666820b3c5c30ae92695c4dae40b1bed9
Eduardo Mateus <eduardo.santos1081@edu.udesc.br>
2025-09-07 23:47:47 -0300

    Exercícios02 – Finalizados

---
+++ Exercicios2/exercicio1oo/exercicio2oo/TesteCarro.java
@@ -0,0 +1,19 @@
+package exercicio2oo.testes;
+
+import exercicio2oo.classes.Carro;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.modelo = "Onix";
+        carro1.marca = "Chevrolet";
+        carro1.ano = 2023;
+        carro1.velocidade = 0.0;
+
+        System.out.println("\n--- Dados do Carro ---");
+        System.out.println("Modelo: " + carro1.modelo);
+        System.out.println("Marca: " + carro1.marca);
+        System.out.println("Ano: " + carro1.ano);
+        System.out.println("Velocidade Atual: " + carro1.velocidade + " km/h");
+    }
+}
